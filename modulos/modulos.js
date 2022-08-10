86a1188874333b22385b3e83641485e44d9e78b6
felipeghidini <lipeghidini18@gmail.com>
2022-08-10 10:48:02 -0300

    Módulos

---
+++ modulos/modulos.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const retangulo_1 = require("./retangulo");
+const circunferencia_1 = require("./circunferencia");
+console.log('Modulo carregado...');
+console.log((0, retangulo_1.areaRetangulo)(2, 4));
+console.log((0, circunferencia_1.areaCircunferencia)(4));
+const { digaOi } = require('./novo');
+console.log(digaOi('Ana'));
+//# sourceMappingURL=modulos.js.map
---
+++ modulos/modulos.ts
@@ -0,0 +1,10 @@
+import { areaRetangulo } from "./retangulo";
+import { areaCircunferencia } from "./circunferencia";
+
+console.log('Modulo carregado...');
+console.log(areaRetangulo(2, 4));
+console.log(areaCircunferencia(4));
+
+const { digaOi } = require('./novo');
+console.log(digaOi('Ana'));
+
